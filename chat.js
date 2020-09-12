b7a466b37e119c10fe137d5dc30c4ef99dd102e7
chengxi.li <chengxi.li@shopee.com>
2020-09-13 00:59:11 +0800

    feat: improve

--- chat.js
+++ chat.js
@@ -1,5 +1,7 @@
 const fs = require('fs-extra');
 
+const deductArray = [1, 1, 2, 3, 4, 4, 5, 7, 9, 12];
+
 module.exports = (server, options, next) => {
   server.route({
     method: 'GET',
@@ -15,10 +17,21 @@ module.exports = (server, options, next) => {
     handler: async (request, reply) => {
       try {
         let uid = request.body.uid || '2';
+        let timestamp = request.body.timestamp;
 
         let data = JSON.parse(fs.readFileSync('./data/data.json', 'utf-8'));
 
         if (data.hasOwnProperty(uid)) {
+          data[uid] = data[uid].map((item, index) => {
+            if (item.mid === 11) {
+              return item;
+            }
+
+            item.msg[item.msg.length - 1].date =
+              timestamp - deductArray[index] * 60;
+
+            return item;
+          });
           reply.send({
             code: 0,
             msg: 'ok',
